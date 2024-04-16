8d4bb71ad51cfe4e230094dffa16f8e12da3b7c1
brazhnyknorman <brazhnyknorman@gmail.com>
2024-04-16 14:05:36 -0400

    Method that will print Board objects

--- lib/chess_print.rb
+++ lib/chess_print.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: false
 
-def print_grid(board, margin_y = board.vertical_margin, margin_x = board.horizontal_margin)
-  grid = board.grid
-
+def print_grid(board, grid = board.grid, margin_y = board.vertical_margin, margin_x = board.horizontal_margin)
   i = 0
-  until i > 8
+  until i > 7
     print "#{margin_y[i]} "
     grid[i].each { |element| print "#{element} " }
     print "\n"
 
     i += 1
   end
+
+  print '  '
+  margin_x.each { |element| print "#{element} " }
+  print "\n"
 end
